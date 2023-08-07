02e190df0b41f92c6969a8b21580a23e7d49f1c2
0h328 <yhkim03280@gmail.com>
2023-08-08 03:09:36 +0900

    FE : buttonsystem

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import ButtonSystem from './components/ButtonSystem';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box style={{ marginTop: 40, marginLeft: 40}}>
+      <Typography variant='h4'>Design System</Typography>
+      <ButtonSystem></ButtonSystem>
+    </Box>
+
+    
   );
 }
 
---
+++ src/components/ButtonSystem/index.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+
+function ButtonSystem (props) {
+  const {onClick} = props;
+  return (
+    <Box style={{ marginLeft: 20 }}> 
+      <Typography variant="h5" style={{ marginTop: 40, fontWeight: "bold"}}>Button</Typography>
+      <Box sx={{ width: '100%'}} style={{ marginTop: 20}}>    
+        <Box style={{ float:"left" }}> 
+          <Stack spacing={2}> 
+            <Typography variant="h6">Default</Typography>
+            <Typography variant="h6">Hover</Typography>
+          </Stack>
+        </Box>        
+        <Stack spacing={2}> 
+          <Button 
+            variant="contained" 
+            style={{ color:"white", width: 100, hegiht: 45, marginLeft: 30}}
+            onClick={onClick}
+          >
+            버튼명
+          </Button>
+          <Button 
+            variant="contained" 
+            style={{ color:"white", width: 100, hegiht: 45, marginLeft: 30}}
+            onClick={onClick}
+          >
+            버튼명
+          </Button>
+        </Stack>
+      </Box>
+    </Box>
+  )
+}
+
+export default ButtonSystem;
